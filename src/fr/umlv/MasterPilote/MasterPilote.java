30dc7d0e7b6063124f2dcce2032399ebbebfe2dd
EMMANUEL <emmanuel@Emmanuel-PC.home>
2013-11-11 16:12:36 +0100

    src and test folder

---
+++ src/fr/umlv/MasterPilote/MasterPilote.java
@@ -0,0 +1,5 @@
+package fr.umlv.MasterPilote;
+
+public class MasterPilote {
+
+}
---
+++ test/fr/umlv/MasterPilote/MasterPiloteTest.java
@@ -0,0 +1,14 @@
+package fr.umlv.MasterPilote;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MasterPiloteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
